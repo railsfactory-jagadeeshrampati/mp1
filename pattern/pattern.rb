28bffdf48d5463b2f3310eb4dfb3410d8795cc0e
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-02 15:10:34 +0530

    pyramid pattern of stars

--- pattern/pattern.rb
+++ pattern/pattern.rb
@@ -2,6 +2,6 @@ puts "enter the number of rows"
 w = gets.to_i
 1.upto(w) do |i|
   spaces = " " * (w-i)
-  stars   = "*" * (i*2 - 1) # Use one fewer dot per line, starting with one dot
+  stars   = "*" * (i*2 - 1) 
   puts  spaces + stars + spaces 
 end
