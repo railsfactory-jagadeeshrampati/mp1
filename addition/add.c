33e8477118d2ca7e5f1e15f269598dab887330ba
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-03 14:37:43 +0530

    removed unwanted foldersaddition/a.out

--- addition/add.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
- int a,b;
- printf("enter any two numbers to add \n");
-scanf("%d%d",&a,&b);
-printf("Sum of entered numbers is %d\n",(a+b));
-return 0;
-}
--- integer/integer.rb
+++
@@ -1,5 +0,0 @@
-a=0
-puts "enter any integer"
-a=gets.chomp.to_i
-puts "Integer that you have entered is" 
-puts a
