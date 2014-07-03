76f09cf0e8c37c459bc008155ca86aae2c959e5d
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-03 15:40:21 +0530

    find an element in array using binary search

---
+++ binary_search/binary_search.rb
@@ -0,0 +1,22 @@
+a = []
+puts "enter the number of elements in array"
+n = gets.to_i
+puts "Enter #{n} numbers"
+for i in 0...n
+   a[i] = gets.to_i
+end
+puts "enter the number that you want to search"
+s = gets.to_i
+first = 0
+last = n-1
+middle = (first+last)/2
+ while first <= last
+     if a[middle] == s
+         puts "#{s} is found at location #{ middle + 1 }"
+          break
+      elsif a[middle] < s
+            first = middle + 1
+       else 
+           last = middle - 1
+ end
+end
