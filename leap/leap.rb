6a431c884bfab32b32a22e0989aa9e9337c32f97
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-03 12:21:16 +0530

    to check leap year or not

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,8 +1,11 @@
 year=""
 puts "enter a year to check if it is a leap year or not \n"
 year=gets.chomp.to_i
-if year%4==0
- puts "\nits a leap year\n"
-else
- puts "\n not leap year \n"
+if year%400==0 || year%4==0
+  puts "#{year} is a leap year"
+  elsif year%100==0
+  puts "#{year} is not a leap year"
+  else
+  puts "#{year} is not a leap year"
 end
+
