5e122675adeb61b72ab3f03fcdcb50f9fe44ebca
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-03 15:26:42 +0530

    search element in array using linear search

---
+++ linear_search/linear_search.rb
@@ -0,0 +1,18 @@
+a = []
+puts "enter the number of elements in array"
+n = gets.to_i
+puts "Enter #{n} numbers"
+for i in 0...n
+   a[i] = gets.to_i
+end
+puts "enter the number that you want to search"
+s=gets.to_i
+c=0
+  for c in 1...a.size
+       if a[c] == s
+         puts "#{s} is present at position #{c+1}"
+        end
+  end
+if c == n
+ puts "#{s} is not found "
+end
