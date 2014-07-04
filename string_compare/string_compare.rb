82cb9e7bc99db6a392b4b4513bc83590a1fbc4b5
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-04 17:47:47 +0530

    compare the given strings

---
+++ string_compare/string_compare.rb
@@ -0,0 +1,9 @@
+puts "enter first string"
+str = gets.chomp
+puts "enter second string"
+str1 = gets.chomp
+if str == str1
+ puts "entered strings are equal"
+else
+ puts "strings are not equal"
+end
