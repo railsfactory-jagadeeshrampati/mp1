2dad342992cb6ad6bd51367b99df192ce0ee035b
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-08 09:26:15 +0530

    print the substring of a given string

--- substring/substring.rb
+++ substring/substring.rb
@@ -1,11 +1,11 @@
 puts "Enter the strings"
 str = gets.chomp
-str.split(' ')
 puts "enter the position and length of substring"
 i = gets.chomp.to_i
 j = gets.chomp.to_i
 puts"The required substring is:"
-for p in i-1..j
-	print "#{str[p].chr}"
+for i in i-1...i-1 + j
+      print "#{str[i].chr}"
 end
 puts " "
+
